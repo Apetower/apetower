6efd8c95fb33f5dd9bf30914e6dd0c0c9fe7d6de
Joe <joe.bowman@gmail.com>
2016-02-02 16:42:41 -0800

    fdsa

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -6,44 +6,45 @@ $(function () {
     var formMessages = $('#form-messages');
 
     // Get the email div.
-    
+
 
     // Set up an event listener for the contact form.
     $(form).submit(function (e) {
             // $('.input-btn').click(function(e) {
             // Stop the browser from submitting the form.
             e.preventDefault();
-        var email = $('#email').val();
-console.log('email value: '+email);
+            var email = $('#email').val();
+            console.log('email value: ' + email);
 
             // Submit the form using AJAX.
             $.ajax({
                     type: 'POST',
                     url: $(form).attr('action'),
                     data: {
-                        email:email
+                        email: email
                     }
                 })
                 .done(function (response) {
                     // Make sure that the formMessages div has the 'success' class.
                     $(formMessages).removeClass('error');
                     $(formMessages).addClass('success');
-
-                    // Clear the form.
-                    $('#email').val('');
-                })
-                .fail(function (data) {
-                        // Make sure that the formMessages div has the 'error' class.
-                        $(formMessages).removeClass('success');
-                        $(formMessages).addClass('error');
-
-
-
-                    })
-            
-// display result
-$(formMessages).text(response);    
+                    // display result
+                    $(formMessages).text(response);
                 });
 
-    });
+            // Clear the form.
+            $('#email').val('');
+        })
+        .fail(function (data) {
+            // Make sure that the formMessages div has the 'error' class.
+            $(formMessages).removeClass('success');
+            $(formMessages).addClass('error');
+            // display result
+            $(formMessages).text(data);
 
+
+        })
+
+
+
+});
