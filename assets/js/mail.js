c93c84a21bd3695e8026a3208eda3f6d6a20f9b6
Joe <joe.bowman@gmail.com>
2016-02-02 16:53:50 -0800

    syntax

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -10,42 +10,42 @@ $(function () {
 
     // Set up an event listener for the contact form.
     $(form).submit(function (e) {
-            // $('.input-btn').click(function(e) {
-            // Stop the browser from submitting the form.
-            e.preventDefault();
-            var email = $('#email').val();
-            console.log('email value: ' + email);
+        // $('.input-btn').click(function(e) {
+        // Stop the browser from submitting the form.
+        e.preventDefault();
+        var email = $('#email').val();
+        console.log('email value: ' + email);
 
-            // Submit the form using AJAX.
-            $.ajax({
-                    type: 'POST',
-                    url: $(form).attr('action'),
-                    data: {
-                        email: email
-                    }
-                })
-                .done(function (response) {
-                    // Make sure that the formMessages div has the 'success' class.
-                    $(formMessages).removeClass('error');
-                    $(formMessages).addClass('success');
-                    // display result
-                    $(formMessages).text(response);
-                    // Clear the form.
-                    $('#email').val('');
-                });
-
-
-        })
-        .fail(function (data) {
-            // Make sure that the formMessages div has the 'error' class.
-            $(formMessages).removeClass('success');
-            $(formMessages).addClass('error');
-            // display result
-            $(formMessages).text(data);
+        // Submit the form using AJAX.
+        $.ajax({
+                type: 'POST',
+                url: $(form).attr('action'),
+                data: {
+                    email: email
+                }
+            })
+            .done(function (response) {
+                // Make sure that the formMessages div has the 'success' class.
+                $(formMessages).removeClass('error');
+                $(formMessages).addClass('success');
+                // display result
+                $(formMessages).text(response);
+                // Clear the form.
+                $('#email').val('');
 
 
-        })
+
+            })
+            .fail(function (data) {
+                // Make sure that the formMessages div has the 'error' class.
+                $(formMessages).removeClass('success');
+                $(formMessages).addClass('error');
+                // display result
+                $(formMessages).text(data);
 
 
+            })
+
+    });
 
 });
